2853139f50fdeed88405c467e2c036fbd175eeb2
CheezItMan <chris@adadevelopersacademy.org>
2019-12-10 13:59:15 -0800

    added validations

--- src/components/NewStudentForm.js
+++ src/components/NewStudentForm.js
@@ -9,6 +9,22 @@ class NewStudentForm extends Component {
       fullName: '',
       email: ''
     }
+    this.validators = {
+      fullName: /.+/,
+      email: /.+@.+\..+/,
+    };
+  }
+
+  validate = (fieldName) => {
+    const value = this.state[fieldName];
+    const validation = this.validators[fieldName];
+
+    if (value.match(validation)) {
+      return true;
+    }
+
+    return false;
+
   }
 
   onFieldChange = (event) => {
@@ -22,6 +38,19 @@ class NewStudentForm extends Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
+
+    let allValid = true;
+
+    Object.keys(this.validators).forEach((key) => {
+      if (!this.state[key].match(this.validators[key])) {
+        allValid = false;
+      }
+    });
+
+    if (!allValid) {
+      return;
+    }
+
 
     const newStudent = {
       name: this.state.fullName,
@@ -39,6 +68,10 @@ class NewStudentForm extends Component {
   }
 
   render () {
+    const emailValid = this.validate('email');
+    const fullNameValid = this.validate('fullName');
+
+
     return (
       <form className="new-student-form" onSubmit={this.onFormSubmit}>
         <div>
@@ -47,6 +80,8 @@ class NewStudentForm extends Component {
             name="fullName"
             value={this.state.fullName}
             onChange={this.onFieldChange}
+            className={fullNameValid ? 'valid' : 'invalid'}
+
           />
         </div>
         <div>
@@ -55,6 +90,8 @@ class NewStudentForm extends Component {
             name="email"
             value={this.state.email}
             onChange={this.onFieldChange}
+            className={emailValid ? 'valid' : 'invalid'}
+
           />
         </div>
         <input
